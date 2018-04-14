26b858ea549093b513b32a4fe6e7885bfac920cf
Jimmy-Johnson <jay4842@gmail.com>
2018-04-14 16:55:39 -0500

    scrapped the check attack
    Still getting bad results, working on a fix currently.

--- genetic_boy/base.py
+++ genetic_boy/base.py
@@ -25,8 +25,20 @@ def generate_state(n=8):
 # thats it nothing special
 
 # generate a board based on a state
+def make_space(state):
+	n = len(state)
+	# make a temporary array to hold the state, making evaluating easy
+	temp_space = [[0 for i in range(n)] for j in range(n)]
+	#print(temp_space)
 
-# print a board based on a temp_space
+	for j in range(0,n): # show the board for now
+		for i in range(0,n):
+			if(j == space[i]):
+				temp_space[i][j] = 1
+
+	return temp_space
+
+# print a board cleanly
 def print_board(space):
 	n = len(space)
 	line = ''
@@ -38,6 +50,7 @@ def print_board(space):
 		line = ''
 	print(board)
 # working on this
+
 def check_attack(space,i,j):
 	print('--------------')
 	print('queen[{},{}]'.format(i,j))
@@ -46,134 +59,7 @@ def check_attack(space,i,j):
 	#print_board(space)
 	attacks = []
 	n = len(space)
-	# START OF CORNER CASES #############################
-	# left top
-	if(i == 0 and j == 0):
-		# sides
-		for x in range(1,n):
-			# check right
-			if(space[i][j+x] == 1):
-				attacks.append([i,j,i,j+x])
-				break # stop looking
-		# check down
-		for x in range(1,n):
-			# check down
-			if(space[i+x][j] == 1):
-				attacks.append([i,j,i+x,j])
-				break # stop looking
-		# diag
-		for x in range(1,n):
-			# check diag down right
-			if(space[i+x][j+x] == 1):
-				attacks.append([i,j,i+x,j+x])
-				break # stop looking
-	#top right #############################
-	elif(i == 0 and j == n-1):
-		# sides
-		for x in range(1,n):
-			# check left
-			if(space[i][j-x] == 1):
-				attacks.append([i,j,i,j-x])
-				break # stop looking
-		# check down
-		for x in range(1,n):
-			# check down
-			if(space[i+x][j] == 1):
-				attacks.append([i,j,i+x,j])
-				break # stop looking
-		# diag
-		for x in range(1,n):
-			# check diag down right
-			if(space[i+x][j-x] == 1):
-				attacks.append([i,j,i+x,j-x])
-				break # stop looking
-	# left bottom #############################
-	elif(i == n-1 and j == 0):
-		# sides
-		for x in range(1,n):
-			# check right
-			if(space[i][j+x] == 1):
-				attacks.append([i,j,i,j+x])
-				break # stop looking
-		# check up
-		for x in range(1,n):
-			# check right
-			if(space[i-x][j] == 1):
-				attacks.append([i,j,i-x,j])
-				break # stop looking
-		# diag
-		for x in range(1,n):
-			# check diag up right
-			if(space[i-x][j+x] == 1):
-				attacks.append([i,j,i-x,j+x])
-				break # stop looking
 	
-	# right bottom #############################
-	elif(i == n-1 and j == n-1):
-		# sides
-		for x in range(1,n):
-			# check left
-			if(space[i][j-x] == 1):
-				attacks.append([i,j,i,j-x])
-				break # stop looking
-		# check up
-		for x in range(1,n):
-			# check up
-			if(space[i-x][j] == 1):
-				attacks.append([i,j,i-x,j])
-				break # stop looking
-		# diag
-		for x in range(1,n):
-			# check diag up left
-			if(space[i-x][j-x] == 1):
-				attacks.append([i,j,i-x,j-x])
-				break # stop looking
-	# END OF CORNERS #############################
-	
-	# SIDES
-
-	#  LEFT SIDE
-	elif((i > 0 and i < n-1) and j == 0):
-		print('check right')
-		for x in range(1,n):
-			print(x)
-			# check right
-			if(space[i][x] == 1):
-				attacks.append([i,j,i,x])
-				break # stop looking
-		# check down
-		print('check down')
-		for x in range(i+1,n):
-			# check down
-			print(x)
-			if(space[x][j] == 1):
-				attacks.append([i,j,x,j])
-				break # stop looking
-		# check up
-		print('check up')
-		for x in range(1,n-(n-i)): 
-			# check up
-			print(x)
-			if(space[i-x][j] == 1):
-				attacks.append([i,j,i-x,j])
-				break # stop looking
-		# diag
-		print('check diag down right')
-		for x in range(i+1,n):
-			print(x)
-			# check diag down right
-			if(space[x][j+x] == 1):
-				attacks.append([i,j,x,j+x])
-				break # stop looking
-		#
-		print('check diag up right')
-		for x in range(1,n-(n-i)):
-			print(x)
-			# check diag up right
-			if(space[i-x][j+x] == 1):
-				attacks.append([i,j,i-x,j+x])
-				break # stop looking
-
 	# if center
 	# check for unique values
 	unique_attks = []
@@ -215,3 +101,6 @@ if __name__ == '__main__':
 
 	fit = cal_fitness(space)
 	print(fit)
+
+
+
