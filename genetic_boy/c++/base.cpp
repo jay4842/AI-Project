56a9c1b7571f2fca9c11b9541c71b16098e38840
Jimmy-Johnson <jay4842@gmail.com>
2018-04-29 20:23:47 -0500

    c++ update

---
+++ genetic_boy/c++/base.cpp
@@ -0,0 +1,17 @@
+// Changing our implementation to c++ ;~; rip
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+
+
+// some helpers
+int randNum(int min, int max){
+	int x = rand() % min +  max;
+	return x;
+}
+
+
+
+int main(){
+	return 0;
+}
---
+++ genetic_boy/c++/state.h
@@ -0,0 +1,23 @@
+// layout our class and provide all the helper functions for it as well
+#ifndef state_H
+#define state_H
+
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class state{
+	private:
+		int n;
+		int *space; // pointer to an array
+	public:
+		state();
+		state(int n);
+		
+
+};
+
+// definitions here
+
+#endif
